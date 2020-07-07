9c23a9d856812cb0438462fb476bb33ca148354f
Kamil Mackow <kamilmackow98@gmail.com>
2020-07-07 05:07:33 +0200

    projects section - removed mutation observer, added flickity on change event listener instead, card redesign

--- src/App.tsx
+++ src/App.tsx
@@ -125,27 +125,27 @@ export let ProjectsCollection: ProjectsProps[] = [
     title: "Music Player",
     description:
       "A music player inspired by CaTRoX theme for Foobar2000. Designed mainly to play several songs from different albums. User can drag, open or add audio files which will then be added to the main playlist. Thanks to JS Media Tags, the album cover for each song can be displayed along with other song information. User can also search for the lyrics of the current song thanks to the Happi API.",
-    progLang: ["Scss", "ReactJS", "TypeScript", "JS MediaTags", "Happi API"],
+    progLang: ["SCSS", "ReactJS", "TypeScript", "JS MediaTags", "Happi API"],
     link: "https://kamilmackow98.github.io/player/",
     code: "https://github.com/kamilmackow98/player/tree/master",
   },
   {
     title: "EcoLogTrans",
     description:
-      "Small brochure website for my cousin and his transport company with some basic features such as Google Maps or contact forms.",
+      "Small brochure website for my cousin's transport company EcoLogTrans, with some basic features like Google Maps, contact forms or an option to choose between two languages that are dynamically displayed on the website but are still rendered on the server side.",
     progLang: ["JavaScript", "PHP", "PHPMailer", "SCSS"],
     link: "https://www.ecologtrans.pl",
   },
   {
     title: "CCV Web APP",
     description:
-      "A web app that allows customers and sellers of the CCV store to take a photo or to record a short video of themselves. Then the user can send it by email or share it on social media like Facebook, Instagram or YouTube.",
+      "A web app that allows customers and sellers of CCV store to take a photo or to record a short video of themselves. User can send a photo or a video by email or share it on social media like Facebook, Instagram or YouTube. Each video is converted thanks to the FFmpeg library to meet the standards of selected social media and is eventually published online.",
     progLang: ["JavaScript", "PHP", "PHPMailer", "Facebook API", "Instagram API", "XAMPP", "FFmpeg"],
   },
   {
     title: "WordPress plugin",
     description:
-      "Modification of an existing WordPress plugin called AmeliaBooking. Main goal was to provide the option to book an appointment for any given product chosen by the user. My part was to add some features to the plugin, improve confirmation message sent to the user and redesign the look on the page but also to link it to the WooCommerce plugin and products available on the website.",
+      "Modification of an existing WordPress plugin called AmeliaBooking. Main goal was to provide an option to book an appointment for any given product chosen by the user. My part was to add some features to the plugin, improve confirmation message sent to the user and redesign the look on the page but also to link it to the WooCommerce plugin and products available on the website.",
     progLang: ["JavaScript", "PHP", "WordPress"],
     link: "https://www.ifonassist.com/prise-de-rendez-vous-reparation-ifonassist/",
   },
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -20,52 +20,58 @@ const Projects = () => {
 
   // after component mount add Flickity on id projectsContainer
   React.useEffect(() => {
-    flickity.current = new Flickity("#projectsContainer", {
+    let projTxts = document.querySelectorAll(".projects__bgText") as NodeListOf<HTMLElement>;
+    let cards = document.querySelectorAll(".card-container") as NodeListOf<HTMLElement>;
+    let flkty = flickity.current as Flickity;
+    let prevIndex: number = 0;
+
+    flkty = new Flickity("#projectsContainer", {
       percentPosition: true,
       dragThreshold: 25,
       pageDots: false,
     });
 
-    let projectsItem = document.getElementsByClassName("projects__item");
-    let cards = document.getElementsByClassName("card-container");
-
-    // custom event listener for changes in DOM
-    const mutationObserver = new MutationObserver(function (mutationsList: any, observer: any) {
-      // for each mutation
-      for (let mutation of mutationsList) {
-        // here there are always two mutations because
-        // JS removes "is-selected" and adds it to another element
-
-        // here if mutation element doesn't have "is-selected" class
-        // checks if first child have "isFlipped" class and if does, removes it
-        if (!mutation.target.classList.contains("is-selected")) {
-          if (mutation.target.firstElementChild.classList.contains("isFlipped")) {
-            mutation.target.firstElementChild.classList.remove("isFlipped");
-          }
+    flkty.on("cellSelect", function () {
+      cards.forEach((card, i) => {
+        if (i !== flkty.selectedIndex) {
+          card.classList.remove("isFlipped");
         }
-      }
-    });
 
-    // for each element observe if changes in attributes like classes, id or aria-labels etc.
-    for (let i = 0; i < projectsItem.length; i++) {
-      mutationObserver.observe(projectsItem[i], { attributes: true, attributeFilter: ["aria-hidden"] });
-    }
+        if (flkty.selectedIndex !== prevIndex) {
+          projTxts.forEach((txt) => {
+            txt.classList.remove("isGlowing");
+          });
+
+          prevIndex = flkty.selectedIndex;
+        }
+      });
+    });
 
     for (let i = 0; i < cards.length; i++) {
       // two buttons in each card
-      let buttons = cards[i].getElementsByClassName("card-button");
+      let buttons = cards[i].querySelectorAll(".card-button");
 
       // for buttons in each card element add event listener and toggle class
       for (let j = 0; j < buttons.length; j++) {
         buttons[j].addEventListener("click", (e) => {
-          e.preventDefault();
-          cards[i].classList.toggle("isFlipped");
+          if (i === flkty.selectedIndex) {
+            e.preventDefault();
+
+            projTxts.forEach((txt) => {
+              txt.classList.toggle("isGlowing");
+            });
+
+            cards[i].classList.toggle("isFlipped");
+          }
         });
       }
     }
   }, [flickity]);
 
-  const contactMe = () => {
+  const contactMe = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    e.preventDefault();
+
     const contactSection = document.getElementsByClassName("contact__section")[0] as HTMLElement;
     const bottom = contactSection.getBoundingClientRect().bottom + window.scrollY;
     const phone = document.querySelector('.contact__section li[data-link="phone"] a') as HTMLElement;
@@ -106,12 +112,9 @@ const Projects = () => {
                 <div className="projects__content face">
                   <div className="projects__title">{project.title}</div>
 
-                  <div className="projects__tech">
-                    <div>Technology used</div>
-                    {project.progLang.join(" - ")}
-                  </div>
+                  <div className="projects__tech">{project.progLang.join(" - ")}</div>
 
-                  <button tabIndex={0} className="card-button">
+                  <button className="card-button">
                     Read More
                     <span className="btn-span btn-span1"></span>
                     <span className="btn-span btn-span2"></span>
@@ -124,32 +127,43 @@ const Projects = () => {
               {/* back side of the card */}
               <div className="card card__back">
                 <div className="projects__content back">
-                  <div className="projects__wrap">
-                    <div>About</div>
-                    <hr />
-                    <div className="projects__desc">{project.description}</div>
-                    <hr />
-                    {project.link ? (
-                      <a className="projects__link" href={`${project.link}`} target="_blank" rel="noopener noreferrer">
-                        <i className="fas fa-search"></i>
-                      </a>
-                    ) : (
-                      <div className="projects__link">
-                        <i className="fas fa-search"></i>
-                      </div>
-                    )}
-                    {project.code ? (
-                      <a className="projects__code" href={`${project.code}`} target="_blank" rel="noopener noreferrer">
-                        <i className="fas fa-code"></i>
-                      </a>
-                    ) : (
-                      <button className="projects__code" onClick={contactMe}>
-                        <i className="fas fa-code"></i>
-                      </button>
-                    )}
-                  </div>
+                  <div className="projects__desc">{project.description}</div>
+                  {project.code ? (
+                    <a className="projects__code" href={`${project.code}`} target="_blank" rel="noopener noreferrer">
+                      <span className="span-label">Code</span>
+                      <i className="fas fa-code"></i>
+                    </a>
+                  ) : (
+                    <a
+                      className="projects__code"
+                      href="#0"
+                      onClick={(e) => {
+                        contactMe(e);
+                      }}
+                    >
+                      <span className="span-label">Contact me for code</span>
+                      <i className="fas fa-code"></i>
+                    </a>
+                  )}
+                  {project.link ? (
+                    <a className="projects__link" href={`${project.link}`} target="_blank" rel="noopener noreferrer">
+                      <span className="span-label">Preview</span>
+                      <i className="fas fa-search"></i>
+                    </a>
+                  ) : (
+                    <a
+                      className="projects__link"
+                      href="#0"
+                      onClick={(e) => {
+                        contactMe(e);
+                      }}
+                    >
+                      <span className="span-label">Contact me for preview</span>
+                      <i className="fas fa-search"></i>
+                    </a>
+                  )}
 
-                  <button tabIndex={0} className="card-button">
+                  <button className="card-button">
                     Go Back
                     <span className="btn-span btn-span1"></span>
                     <span className="btn-span btn-span2"></span>
